0f2691da695bac9c90de4e2d5c99d2d34f736dec
albertoOlmo1233 <albertoolmofp@ibf.cat>
2025-02-06 17:47:39 +0100

    Header casi completo

--- resources/js/layouts/Nav.vue
+++ resources/js/layouts/Nav.vue
@@ -1,30 +1,27 @@
 <template>
     <nav class="navbar navbar-expand-md navbar-light fondoNav shadow-sm">
-        <div class="container p-0 m-0">
-            <img src="images/logo.svg" class="logo-size" alt="logo">
-            <router-link to="/" class="navbar-brand">Soomfy</router-link>
-            <a class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </a>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mt-2 mt-lg-0 me-auto mb-2 mb-lg-0">
-                    <LocaleSwitcher />
-                </ul>
-                <ul class="navbar-nav mt-2 mt-lg-0 ms-auto">
-                        <button class="primary-button">Como funciona</button>
-                        <router-link to="/" class="nav-link d-flex align-items-center" aria-current="page">
-                            <img src="images/chat.svg" class="icons-size" alt="">
-                            <p>Chat</p>
-                        </router-link>
-                        <router-link to="/" class="nav-link d-flex align-items-center" aria-current="page">
-                            <img src="images/favoritos.svg" class="icons-size" alt="">
-                        </router-link>
+        <div class="container p-0 m-0 d-flex justify-content-between">
+            <div class="d-flex w-50 align-items-center">
+                <img src="images/logo.svg" class="logo-size" alt="logo">
+                <router-link to="/" class="navbar-brand">Soomfy</router-link>
+            </div>
+            <div class="d-flex w-50 justify-content-end align-items-center">
+                <ul class="navbar-nav align-items-center gap-2">
+                    <button class="primary-button">Como funciona</button>
+                    <router-link to="/chat" class="nav-link d-flex align-items-center gap-2">
+                        <img src="images/chat.svg" class="icons-size" alt="">
+                        <p class="textSize-chat">Chat</p>
+                    </router-link>
+                    <router-link to="/" class="nav-link d-flex align-items-center">
+                        <img src="images/favoritos.svg" class="icons-size" alt="">
+                    </router-link>
                     <template v-if="!authStore().user?.name">
-                            <router-link class="nav-link" to="/register"><img src="images/logoUser.svg" class="icons-size"></router-link>
+                        <router-link class="nav-link" to="/register">
+                            <img src="images/logoUser.svg" class="icons-size">
+                        </router-link>
                     </template>
                     <li v-if="authStore().user?.name" class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
                             {{ authStore().user?.name }}
                         </a>
                         <ul class="dropdown-menu dropdown-menu-end">
@@ -34,8 +31,8 @@
                             <li><a class="dropdown-item" href="javascript:void(0)" @click="logout">Logout</a></li>
                         </ul>
                     </li>
-                    <router-link to="/" class="nav-link d-flex align-items-center" aria-current="page">
-                        <button class="d-flex secondary-button">
+                    <router-link to="/" class="nav-link d-flex align-items-center">
+                        <button class="d-flex secondary-button gap-2">
                             <img src="images/agregar.svg" class="icon-plus" alt="">
                             <p>Anuncia y vende</p>
                         </button>
@@ -45,6 +42,7 @@
         </div>
     </nav>
 </template>
+
 
 <script setup>
 
