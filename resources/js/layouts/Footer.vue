9c7bd17612372cd8209e8c3b15e1d2d9622290c2
albertoOlmo1233 <albertoolmofp@ibf.cat>
2025-02-07 19:57:15 +0100

    Header mejorado, comenzando Home y footer

---
+++ resources/js/layouts/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer class="navbar navbar-expand-md navbar-light shadow-sm justify-content-center">
+        <div class="container p-0 m-0 d-flex justify-content-center">
+            <div class="d-flex w-50 align-items-center justify-content-center">
+                <p>© 2025 Soomfy. Compra venta de segunda mano</p>
+            </div>
+            
+        </div>
+    </footer>
+</template>
+
+
+<script setup>
+
+import useAuth from "@/composables/auth";
+import LocaleSwitcher from "../components/LocaleSwitcher.vue";
+import { authStore } from "../store/auth";
+
+const { processing, logout } = useAuth();
+</script>
