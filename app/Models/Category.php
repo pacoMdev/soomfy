91aab9d767c8c42721e03ccece4fed77f40d359a
albertoOlmo1233 <albertoolmofp@ibf.cat>
2025-02-26 16:35:59 +0100

    Revert "path personalizado spatie media"
    This reverts commit e378f7778da73ebffef959cde18e74336056fd89.

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -36,14 +36,8 @@ class Category extends Model implements HasMedia
     {
         $this->addMediaCollection('category_image')
             ->useDisk('public');
-    }
 
-    // Sobrescribir el método que determina la ruta de almacenamiento
-    public function getMediaFolderPath(): string
-    {
-        return 'categories/' . $this->id;
     }
-
 
 
 }
