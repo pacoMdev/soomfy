e378f7778da73ebffef959cde18e74336056fd89
albertoOlmo1233 <albertoolmofp@ibf.cat>
2025-02-26 10:10:33 +0100

    path personalizado spatie media

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -36,8 +36,14 @@ class Category extends Model implements HasMedia
     {
         $this->addMediaCollection('category_image')
             ->useDisk('public');
-
     }
+
+    // Sobrescribir el método que determina la ruta de almacenamiento
+    public function getMediaFolderPath(): string
+    {
+        return 'categories/' . $this->id;
+    }
+
 
 
 }
