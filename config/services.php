b1786593a5f2ce1c1441fcd968cef8d83c09fc80
dev3Paco <fraciscomorales02@gmail.com>
2025-04-02 22:57:39 +0200

    Update services.php

--- config/services.php
+++ config/services.php
@@ -15,20 +15,20 @@ return [
     */
 
     'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
-        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
-        'scheme' => 'https',
+        'domain'            => env('MAILGUN_DOMAIN'),
+        'secret'            => env('MAILGUN_SECRET'),
+        'endpoint'          => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+        'scheme'            => 'https',
     ],
 
     'postmark' => [
-        'token' => env('POSTMARK_TOKEN'),
+        'token'             => env('POSTMARK_TOKEN'),
     ],
 
     'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
-        'secret' => env('AWS_SECRET_ACCESS_KEY'),
-        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+        'key'               => env('AWS_ACCESS_KEY_ID'),
+        'secret'            => env('AWS_SECRET_ACCESS_KEY'),
+        'region'            => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
     'google' => [
         'client_id'         => env('GOOGLE_CLIENT_ID'),
