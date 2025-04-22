af0c8cec80e35badbd2946c77d470974a8377500
dev3Paco <fraciscomorales02@gmail.com>
2025-04-22 11:36:08 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (app()->environment('production')) {
+        if (app()->environment('prod')) {
             URL::forceScheme('https');
         }
     }
