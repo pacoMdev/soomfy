05c8ecf53f58bd9d761f980a8f1d4047706c38a3
albertoOlmo1233 <albertoolmofp@ibf.cat>
2025-04-25 17:36:44 +0200

    Hosting

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bienvenido a {{ config('app.name') }}</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <style>
         body {
             font-family: Arial, sans-serif;
